f2920b0fdc507757cd64d12e0a7c1a64fa3a50c6
Jesper Mikkelsen Post <jesp6544@edu.eal.dk>
2017-02-08 11:19:18 +0100

    Might?

---
+++ FractalServer/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace FractalServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.factalPictureBox = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.ClientDatagridview = new System.Windows.Forms.DataGridView();
+            this.IPTxtbox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ThreadsTxtbox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DisconnectBtn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IPConnectTxtbox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.RenderBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.factalPictureBox)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientDatagridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(508, 526);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.factalPictureBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(500, 500);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // factalPictureBox
+            // 
+            this.factalPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.factalPictureBox.Name = "factalPictureBox";
+            this.factalPictureBox.Size = new System.Drawing.Size(500, 500);
+            this.factalPictureBox.TabIndex = 0;
+            this.factalPictureBox.TabStop = false;
+            this.factalPictureBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.factalPictureBox_MouseDoubleClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.ClientDatagridview);
+            this.tabPage2.Controls.Add(this.ConnectBtn);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.IPConnectTxtbox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(500, 500);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ClientDatagridview
+            // 
+            this.ClientDatagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ClientDatagridview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IPTxtbox,
+            this.ThreadsTxtbox,
+            this.DisconnectBtn});
+            this.ClientDatagridview.Location = new System.Drawing.Point(42, 161);
+            this.ClientDatagridview.Name = "ClientDatagridview";
+            this.ClientDatagridview.Size = new System.Drawing.Size(343, 150);
+            this.ClientDatagridview.TabIndex = 4;
+            // 
+            // IPTxtbox
+            // 
+            this.IPTxtbox.HeaderText = "IP";
+            this.IPTxtbox.Name = "IPTxtbox";
+            this.IPTxtbox.ReadOnly = true;
+            // 
+            // ThreadsTxtbox
+            // 
+            this.ThreadsTxtbox.HeaderText = "Threads";
+            this.ThreadsTxtbox.Name = "ThreadsTxtbox";
+            this.ThreadsTxtbox.ReadOnly = true;
+            // 
+            // DisconnectBtn
+            // 
+            this.DisconnectBtn.HeaderText = "Disconnect";
+            this.DisconnectBtn.Name = "DisconnectBtn";
+            this.DisconnectBtn.ReadOnly = true;
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Location = new System.Drawing.Point(87, 45);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConnectBtn.TabIndex = 3;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP of render pc:";
+            // 
+            // IPConnectTxtbox
+            // 
+            this.IPConnectTxtbox.Location = new System.Drawing.Point(87, 19);
+            this.IPConnectTxtbox.Name = "IPConnectTxtbox";
+            this.IPConnectTxtbox.Size = new System.Drawing.Size(100, 20);
+            this.IPConnectTxtbox.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(527, 146);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(96, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // RenderBtn
+            // 
+            this.RenderBtn.Location = new System.Drawing.Point(527, 117);
+            this.RenderBtn.Name = "RenderBtn";
+            this.RenderBtn.Size = new System.Drawing.Size(96, 23);
+            this.RenderBtn.TabIndex = 2;
+            this.RenderBtn.Text = "Render";
+            this.RenderBtn.UseVisualStyleBackColor = true;
+            this.RenderBtn.Click += new System.EventHandler(this.RenderBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(527, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(527, 88);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(96, 23);
+            this.SaveBtn.TabIndex = 4;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 551);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RenderBtn);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "f";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.factalPictureBox)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientDatagridview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox factalPictureBox;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button ConnectBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IPConnectTxtbox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button RenderBtn;
+        private System.Windows.Forms.DataGridView ClientDatagridview;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IPTxtbox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ThreadsTxtbox;
+        private System.Windows.Forms.DataGridViewButtonColumn DisconnectBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SaveBtn;
+    }
+}
+
---
+++ FractalServer/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Drawing.Imaging;
+
+namespace FractalServer
+{
+    public partial class Form1 : Form
+    {
+        public List<Worker> RenderFarm = new List<Worker>();
+        public Thread ClientThread;
+		public double xmin = -2.1;
+		public double ymin = -1.3;
+		public double xmax = 1;
+		public double ymax = 1.3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Connect();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"Could not connect to IP");
+            }
+        }
+
+        public void Connect()
+        {
+                IPAddress remoteIpAddress;
+                if (IPAddress.TryParse(IPConnectTxtbox.Text,out remoteIpAddress)) //Checks if field can be formatted as an IP
+                {
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(remoteIpAddress.ToString(), 9001);
+                    byte[] bytes = new byte[1024];
+                    socket.Receive(bytes);
+                    Worker temp = new Worker()
+                    {
+                        IP = remoteIpAddress,
+                        Socket = socket,
+                        Threads = Convert.ToUInt16(bytes)
+                    };
+                    RenderFarm.Add(temp);
+                    ClientThread = new Thread(() =>
+                    {
+                        Resive(temp);
+                    });
+                    ClientThread.Start();
+                    ClientDatagridview.Rows.Add(temp.IP.ToString(), temp.Threads.ToString());
+                }
+                else
+                    MessageBox.Show(@"IP is wrongly formattet");
+        }
+
+        public void Send()
+        {
+            foreach (Worker worker in RenderFarm)
+            {
+                //TODO: make a method that returns the next job
+                Job job = new Job();
+                string jobString = JsonConvert.SerializeObject(job);
+                Byte[] jobByte = Encoding.ASCII.GetBytes(jobString);
+                worker.Socket.Send(jobByte);
+            }
+        }
+
+        public void Resive(Worker worker)
+        {
+            try
+            {
+                byte[] bytes = new byte[1024];
+                while (true)
+                {
+                    worker.Socket.Receive(bytes);
+                    string pieceString = Encoding.ASCII.GetString(bytes);
+                    Piece piece = JsonConvert.DeserializeObject<Piece>(pieceString);
+                    //foreach (var i in ClientDatagridview)
+                    //{
+                        
+                    //}
+                    int tempNum = piece.ID*(1000);
+
+                }
+            }
+            catch (Exception)
+            {
+                //TODO: make a popup that askes to remove the tread or retry
+                Thread.CurrentThread.Abort();
+            }
+        }
+
+        private void RenderBtn_Click(object sender, EventArgs e)
+        {
+			Job bla = new Job()
+			{
+				ID = 1,
+				ymin = ymin,
+				ymax = ymax,
+				xmax = xmax,
+				xmin = xmin,
+				height = factalPictureBox.Height,
+				width = factalPictureBox.Width
+			};
+			factalPictureBox.Image = Render(bla);
+			factalPictureBox.Update();
+        }
+		public Bitmap Render(Job job)
+		{
+			//MessageBox.Show(xmin + "\n" + xmax + "\n" + ymin + "\n" + ymax + "\n");
+			// Holds all of the possible colors
+  			Color[] cs = new Color[256];
+
+			//var temp = (xmax - ymax)/2;
+			// Creates the Bitmap we draw to
+			Bitmap b = new Bitmap(job.width, job.height);
+			// From here on out is just converted from the c++ version.
+			double x, y, x1, y1, xx, intigralX, intigralY = 0.0;
+
+			int looper, s, z = 0;
+		    //xmin = -2.1;//Sx; // Start x value, normally -2.1
+		    //ymin = -1.5;//Sy; // Start y value, normally -1.3
+		    //xmax = 0.9;//Fx; // Finish x value, normally 1
+		    //ymax = 1.5;//Fy; // Finish y value, normally 1.3
+			intigralX = (xmax - xmin) / b.Width; // Make it fill the whole window
+			intigralY = (ymax - ymin) / b.Height;
+			x = xmin;
+
+			for (s = 1; s < b.Width; s++)
+			{
+				y = ymin;
+				for (z = 1; z < b.Height; z++)
+				{
+					x1 = 0;
+					y1 = 0;
+					looper = 0;
+					while (looper < 250 && Math.Sqrt((x1 * x1) + (y1 * y1)) < 2)
+					{
+						looper++;
+						xx = (x1 * x1) - (y1 * y1) + x;
+						y1 = 2 * x1 * y1 + y;
+						x1 = xx;
+					}
+
+					// Get the percent of where the looper stopped
+					double perc = looper / (250.0);
+					// Get that part of a 255 scale
+					int val = ((int)(perc * 255));
+					// Use that number to set the color
+				    Color color = Color.FromArgb(val, 0, 0);
+					b.SetPixel(s, z, color);
+					y += intigralY;
+				}
+				x += intigralX;
+			}
+			return b;
+
+			//this.BackgroundImage = (Image)bq; // Draw it to the form
+    }
+
+        private void factalPictureBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+			//MessageBox.Show(e.Y +"");
+			double temp = (xmax - xmin) / 4;
+			//MessageBox.Show("(" + xmax + "-" + xmin + ")" + "*" + "(" + e.X + "/"+ factalPictureBox.Width +")"+ "=" + (xmax - xmin) * (e.X / factalPictureBox.Width) + "");
+			//MessageBox.Show(e.X / 500 + "");
+			//MessageBox.Show(e.X + "")
+			double tempX = xmin + (xmax - xmin) * (Convert.ToDouble(e.X) / factalPictureBox.Width);
+			double tempY = ymin + (ymax - ymin) * (Convert.ToDouble(e.Y) / factalPictureBox.Height);
+			xmin = tempX - temp;
+			xmax = tempX + temp;
+			ymin = tempY - temp;
+			ymax = tempY + temp;
+			Job job = new Job();
+			if (false)  //This piece of code is for makin the jobs for rendering and it not done at this point in time. 
+			{
+				double halfHeight = ymax - (ymax - ymin) / 2;
+				double jobXLenth = temp * 4 / RenderFarm.Count;
+				for (ushort i = 0; i < RenderFarm.Count * 2; i++)
+				{  
+					double tmp;
+					if (i > RenderFarm.Count) { tmp = halfHeight; }
+					else { tmp = ymin; }
+					job.ID = i;
+					job.ymin = tmp;
+					job.ymax = ymax;
+					job.xmax = xmin + i * jobXLenth % RenderFarm.Count;
+					job.xmin = xmin + (i - 1) * jobXLenth % RenderFarm.Count;
+					job.height = factalPictureBox.Height / 2;
+					job.width = factalPictureBox.Width / RenderFarm.Count;
+				}
+			}
+			factalPictureBox.Image = Render(job);
+			factalPictureBox.Update();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        xmin = -2.1;
+        ymin = -1.5;
+        xmax = 0.9;
+        ymax = 1.5;
+    }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+			Job bla = new Job()
+			{
+				ID = 2,
+				ymin = ymin,
+				ymax = ymax,
+				xmax = xmax,
+				xmin = xmin,
+				height = 1200,
+				width = 1200
+			};
+			Render(bla).Save("myfile2.png", ImageFormat.Png);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Job
+    {
+        public ushort ID { get; set; }
+		//Some var(s) that carry the calculation
+		public double xmin { get; set; }
+		public double xmax { get; set; }
+		public double ymin { get; set; }
+		public double ymax { get; set; }
+		public int height { get; set; }
+		public int width { get; set; }
+	}
+
+    public class Piece
+    {
+        public ushort ID { get; set; }  //same as Job ID  //Can handle up to 16383 total cores in renderfarm
+        //TODO: might add offset to enable transfer while going
+        public List<Pixel> pixels { get; set; }
+    }
+    public class Pixel
+    {
+        public uint Placement { get; set; } 		//Int is enough for 1 job to handle 65.000x65.000 picture, no need to go higher really..
+        public Color color { get; set; }  		//Is it more efficient to use 3 ints as RGB?  //TODO: test this
+    }
+    public class Worker
+    {
+        public IPAddress IP { get; set; }
+        public ushort Threads { get; set; }  	//I guess the program won't incounter a machine with over 32,767 threads ¯\_(ツ)_/¯
+		public sbyte Progress { get; set; } 	//Only goes from 0-100 (%) so might as well save 8bits..
+        public Socket Socket { get; set; }
+    }
+
+}
