2a699571ee6c196b7f83907b64f1c3e3e44485b3
jesp6544 <jesp6544@live.dk>
2017-02-08 12:20:52 +0100

    Started working on client. Render now takes Jobs (just like the Mexicans!)

--- FractalClient/Program.cs
+++ FractalClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,25 @@ namespace FractalClient
 {
     class Program
     {
+		static IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 9001);
+		static Socket host = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         static void Main(string[] args)
         {
-        }
+			Console.WriteLine("This is the render client program");
+			Console.WriteLine("Please wait make sure the render host is running");
+			Console.WriteLine("Trying to connect..");
+			Connect();
+		}
+		static void Connect() {
+			host.Bind(localEndPoint);
+			host.Listen(100);
+			host = host.Accept();
+		}
+		static void FirstContact() {
+			string msg = "";
+			byte[] msgbyte = Encoding.ASCII.GetBytes(msg);
+			host.Send(msgbyte);
+		}
+		//byte[] msgByte = Encoding.ASCII.GetBytes(msg);
     }
 }
