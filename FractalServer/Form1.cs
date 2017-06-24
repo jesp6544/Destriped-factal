f821c77862bc393afc8eff8d91df0f0df581444c
jesp6544 <jesp6544@live.dk>
2017-06-24 14:00:11 +0200

    Test

--- FractalServer/Form1.cs
+++ FractalServer/Form1.cs
@@ -423,17 +423,17 @@ namespace FractalServer
 		}
 	}
 
-	public class Job
-	{
-		public ushort ID { get; set; }
-		//Some vars that carry the calculation and size
-		public double xmin { get; set; }
-		public double xmax { get; set; }
-		public double ymin { get; set; }
-		public double ymax { get; set; }
-		public int height { get; set; }
-		public int width { get; set; }
-	}
+	// public class Job
+	// {
+	// 	public ushort ID { get; set; }
+	// 	//Some vars that carry the calculation and size
+	// 	public double xmin { get; set; }
+	// 	public double xmax { get; set; }
+	// 	public double ymin { get; set; }
+	// 	public double ymax { get; set; }
+	// 	public int height { get; set; }
+	// 	public int width { get; set; }
+	// }
 
 	public class Piece
 	{
