9e85955c0af0a7d295ffaf9e11b4b3968c2ab227
jmp <Jmp@ditmer.dk>
2020-05-10 22:12:11 +0200

    #new GUI example project added

---
+++ GuiClient/Logic.cs
@@ -0,0 +1,69 @@
+﻿using System.Drawing;
+using Core;
+using Core.Models;
+
+namespace Gui_client
+{
+    class Logic
+    {
+        private readonly IRender _render;
+        private Job _currentJob;
+        
+        public Logic(int imgWidth, int imgHeight)
+        {
+            _render = new Render(255);
+            _currentJob = new Job
+            {
+                Height = imgHeight,
+                Width = imgWidth,
+                XMax = 1,
+                XMin = -3,
+                YMax = 2,
+                YMin = -2
+            };
+        }
+
+        public void Zoom(double percentFromRight, double percertFromTop)
+        {
+            var temp = (_currentJob.XMax - _currentJob.XMin) / 4;
+            var tempX = _currentJob.XMin + (_currentJob.XMax - _currentJob.XMin) * percentFromRight;
+            var tempY = _currentJob.YMin + (_currentJob.YMax - _currentJob.YMin) * percertFromTop;
+            _currentJob = new Job
+            {
+                Width = _currentJob.Width,
+                Height = _currentJob.Height,
+                XMin = tempX - temp,
+                XMax = tempX + temp,
+                YMin = tempY - temp,
+                YMax = tempY + temp
+            };
+        }
+
+        public void ModifyJob(int xMax, int xMin, int yMax, int yMin)
+        {
+            _currentJob = new Job
+            {
+                Height = _currentJob.Height,
+                Width = _currentJob.Width,
+                XMax = xMax,
+                XMin = xMin,
+                YMax = yMax,
+                YMin = yMin
+            };
+        }
+
+        public Bitmap GetImage(int? overwriteWidth = null, int? overwriteHeight = null)
+        {
+            var job = _currentJob;
+            if (overwriteHeight != null)
+            {
+                job.Height = (int) overwriteHeight;
+            }
+            if (overwriteWidth != null)
+            {
+                job.Width = (int) overwriteWidth;
+            }
+            return _render.GetMandlebrot(job);
+        }
+    }
+}
---
+++ GuiClient/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace Gui_client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Logic _logic;
+        public MainWindow()
+        {
+            Console.WriteLine("test");
+            InitializeComponent();
+            _logic = new Logic((int)RenderImage.Width,(int)RenderImage.Height); // TODO get this from the actual img
+            RenderImageToGui();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _logic.ModifyJob(1, -3, 2, -2);
+            RenderImageToGui();
+        }
+
+        private void Img(object sender, MouseButtonEventArgs e)
+        {
+            _logic.Zoom(e.GetPosition(RenderImage).X / RenderImage.Width,e.GetPosition(RenderImage).Y / RenderImage.Height);
+            RenderImageToGui();
+        }
+
+        private void RenderImageToGui()
+        {
+            RenderImage.Source = Convert(_logic.GetImage());
+        }
+        
+        /// <summary>
+        /// Takes a bitmap and converts it to an image that can be handled by WPF ImageBrush
+        /// </summary>
+        /// <param name="src">A bitmap image</param>
+        /// <returns>The image as a BitmapImage for WPF</returns>
+        public BitmapImage Convert(Bitmap src)
+        {
+            var ms = new MemoryStream();
+            src.Save(ms, ImageFormat.Bmp);
+            var image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var result = _logic.GetImage(2000, 2000);
+            result.Save("Saved_image.png", ImageFormat.Png);
+        }
+    }
+}
